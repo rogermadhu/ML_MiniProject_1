cdfb3d4dcb6f7025a068edce2f7ff7ef245e8ab3
Jasmine Wang <jasminewang76@gmail.com>
2019-01-20 18:58:37 -0500

    using counter

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 import string
 import operator
+from collections import Counter
 
 def read_json_file():
     file = open('./data/proj1_data.json', 'r')
@@ -10,29 +11,15 @@ def read_json_file():
     json_data = json.loads(data)
     return json_data
 
-def to_lower(str):
-    return str.lower()
+def process_string(str):
+    return str.split(' ').lower()
 
-def split(str):
-    return str.split(' ')
-
-def preprocess_text(jsonContent, topMostFreqWords):
-    if len(jsonContent) > 0:
-        textDic = {}
-        for x in jsonContent:
-            wordsList = split(to_lower(x['text']))
-            for word in wordsList:
-                if word not in textDic:
-                    textDic[word] = 1
-                else:
-                    textDic[word]+= 1
-        sortedWordsList = sorted(textDic.items(), key=operator.itemgetter(1), reverse=True)
-        
-        retDic = {}
-        for i in range(0, topMostFreqWords):
-            retDic[sortedWordsList[i][0]] = sortedWordsList[i][1]
-            # print(sortedWordsList[i])
-    return retDic
+def count_top_words(train_data):
+    word_freq = Counter()
+    for line in train_data:
+        word_list = process_string(line['text'])
+        word_freq.update(word_list)
+    return dict(word_freq.most_common(160))
 
 def split_data(jData, start, stop):
     temp = []
@@ -55,8 +42,9 @@ def main():
     test_data = split_data(data, 11000, 12000)
     print("Created testing dataset. Sample from validation data: {}").format(test_data[0])
 
-    print("Preprocessing training data")
-    preprocess_text(train_data, 160)
+    print("Finding top words")
+    word_frequency = count_top_words(train_data)
+    # print(word_frequency)
 
 if __name__ == '__main__':
     main()
